b1d7e65415ffa82a79b5a80653d682413b188440
Alex Zhai <zhaialex9@gmail.com>
2019-10-31 20:23:55 -0700

    add all audios, categorize images, and add recordings to cecelia page

--- jan.js
+++ jan.js
@@ -16,7 +16,7 @@ var config = {
         link: {
           href: "javascript:showCeciliaInfo();"
         },
-        image: "images/cecilia.jpg"
+        image: "images/small/cecilia.jpg"
     },
 
     janet = {
@@ -27,7 +27,7 @@ var config = {
         link: {
           href: "javascript:showJanInfo();"
         },
-        image: "images/jan.jpg"
+        image: "images/small/jan.jpg"
     },
 
     dixon = {
@@ -39,7 +39,7 @@ var config = {
         link: {
           href: "javascript:showDixonInfo();"
         },
-        image: "images/dixon.jpg"
+        image: "images/small/dixon.jpg"
     },
 
 
@@ -78,4 +78,112 @@ function showDixonInfo() {
   else {
     x.style.display = 'none';
   }
+}
+
+function playWord1() {
+  var snd = new Audio("audios/word1.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord2() {
+  var snd = new Audio("audios/word2.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord3() {
+  var snd = new Audio("audios/word3.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord4() {
+  var snd = new Audio("audios/word4.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord5() {
+  var snd = new Audio("audios/word5.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord6() {
+  var snd = new Audio("audios/word6.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord7() {
+  var snd = new Audio("audios/word7.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord8() {
+  var snd = new Audio("audios/word8.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord9() {
+  var snd = new Audio("audios/word9.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord10() {
+  var snd = new Audio("audios/word10.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord11() {
+  var snd = new Audio("audios/word11.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord12() {
+  var snd = new Audio("audios/word12.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord13() {
+  var snd = new Audio("audios/word13.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord14() {
+  var snd = new Audio("audios/word14.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord15() {
+  var snd = new Audio("audios/word15.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord16() {
+  var snd = new Audio("audios/word16.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord17() {
+  var snd = new Audio("audios/word17.m4a");
+  snd.play();
+  snd.currentTime=0;
+}
+
+function playWord18() {
+  var snd = new Audio("audios/word18.m4a");
+  snd.play();
+  snd.currentTime=0;
 }
